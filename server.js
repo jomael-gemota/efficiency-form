7a9988e1e5f565e4b83150ae3bae9b72b1219d13
Jomael Gemota <jomael@outdoorequipped.com>
2020-07-08 03:46:06 +0800

    local-dell: add heroku prerequisites

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const key = require('ckey');
+const cors = require('cors');
 
 const userRouter = require('./api/user/user.router');
 const formRouter = require('./api/form/form.router');
 
 const app = express();
-const port = key.SERVER_PORT;
+app.set('port', process.env.PORT || key.SERVER_PORT);
 
 app.use(express.json());
-
 app.use('/api/user', userRouter);
 app.use('/api/form', formRouter);
+app.use(cors());
 
-app.listen(port, () => {
-    console.log(`Server Status: Port (${port})`);
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Server Status: Port (${app.get('port')})`);
 });
